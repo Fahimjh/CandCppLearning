3fb3da1277150338a0dde530070985489e7f3d23
fahimjh001 <fahimjh001@gmail.com>
2024-11-19 23:23:52 +0600

    previous practice added

---
+++ anisvaicpp11.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num1,num2;
+    float div;
+    cout<<"Enter two number: ";
+    cin>>num1>>num2;
+    cout<<"Addition: "<<num1+num2;
+    cout<<"\nSubtraction: "<<num1-num2;
+    cout<<"\nMultiplication: "<<num1*num2;
+    cout<<"\nDivision: "<<num1/num2;
+}
