ccad15e70e15162a9af0c30218cbd928f50168a6
fahimjh001 <fahimjh001@gmail.com>
2024-11-20 23:57:34 +0600

    previous practice added

---
+++ anisvaicpp52.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b){
+    int sum=a+b;
+    return sum;
+}
+int main(){
+    int a,b,sum;
+    cout<<"Enter two int number: ";
+    cin>>a>>b;
+    sum=add(a,b);
+    cout<<"Your sum is: "<<sum;
+}
