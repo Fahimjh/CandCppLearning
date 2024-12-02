1eba9bec795ace1c0ab6c6a12931bb7e08aec6aa
fahimjh001 <fahimjh001@gmail.com>
2024-12-02 12:02:15 +0600

    w3rsptr3 first half done

---
+++ harrycpp/w3ptrprtc3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    cout<<"Pointer : Demonstrate the use of & and * operator :"<<endl<<"--------------------------------------------------------"<<endl;
+    int m=300;
+    double fx= 300.600006;
+    char cht='z';
+
+    int *ptr1=&m;
+    double *ptr2=&fx;
+    char *ptr3=&cht;
+
+    cout<<"m = "<<m<<endl;
+    cout<<setprecision(10)<<"fx = "<<fx<<endl;
+    cout<<"cht = "<<cht<<endl;
+
+    cout<<" Using & operator :"<<endl<<"-------------------------"<<endl;
+    cout<<"address of m = "<<&m<<endl;
+    cout<<"address of fx = "<<&fx<<endl;
+    cout<<"address of cht = "<<&cht<<endl;
+
+    cout<<" Using & and * operator :"<<endl<<"-------------------------"<<endl;
+    cout<<"value at address of m = "<<*ptr1<<endl;
+    cout<<"address of fx = "<<*ptr2<<endl;
+    cout<<"address of cht = "<<*ptr3<<endl;
+}
