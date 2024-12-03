69a50377a9709936bb858c9ffdabc1a81f3ed7e8
fahimjh001 <fahimjh001@gmail.com>
2024-12-03 20:36:26 +0600

    pointer added with array

--- harrycpp/arrpoinharry.cpp
+++ harrycpp/arrpoinharry.cpp
@@ -6,4 +6,10 @@ int main(){
     for(int i=0;i<5;i++){
         cout<<"Array["<<i<<"]="<<arr[i]<<endl;
     }
+    int *ptr=arr;
+    cout<<"*ptr= "<<*(ptr)<<endl;
+    cout<<"*(ptr+1)= "<<*(ptr+1)<<endl;
+    cout<<"*(ptr+2)= "<<*(ptr+2)<<endl;
+    cout<<"*(ptr+3)= "<<*(ptr+3)<<endl;
+    cout<<"*(ptr+4)= "<<*(ptr+4)<<endl;
 }
