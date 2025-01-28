5508c8b7e396f8a4c11ae120767c7b296411d14a
fahimjh001 <fahimjh001@gmail.com>
2025-01-29 00:34:06 +0600

    harry 37 single inheritance with private

---
+++ harrycpp/harry37Inherit.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+    int id;
+    string type;
+    public:
+    employee(){
+        id=34;
+        cout<<"Enter your department: ";
+        getline(cin,type);
+    }
+    void show(){
+        cout<<"Employee id: "<<id<<endl;
+        cout<<"Employee type"<<type<<endl;
+    }
+    ~employee(){}
+};
+
+class programmer:employee{
+    int pid;
+    float salary;
+    public:
+    programmer(int id,int sal){
+        pid=id;
+        salary=sal;
+    }
+    void show(){
+        employee::show();
+        cout<<"Programmer id: "<<pid<<endl;
+        cout<<"Programmer salary: "<<salary<<endl;
+    }
+    ~programmer(){}
+};
+
+int main(){
+    //employee e(688,80000);
+    programmer p1(688,80000);
+    p1.show();
+    return 0;
+}
