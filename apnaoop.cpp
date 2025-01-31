265f9646eda83d150058c7558d2cc3f7d70c2d76
fahimjh001 <fahimjh001@gmail.com>
2025-01-31 12:34:22 +0600

    apna oop added till non paramterized constructor

--- apnaoop.cpp
+++ apnaoop.cpp
@@ -6,6 +6,10 @@ private:
     float salary;
 
 public:
+
+    Teacher(){
+        cout<<"Hello I am a non parameterized constructor\n";
+    }
     string name,dept,subject;
 
     //setter
@@ -18,14 +22,24 @@ public:
     }
 };
 /*
-Encapsulation means wrapping up attributes and members in a single unit
+                        Encapsulation 
+means wrapping up attributes and members in a single unit
 eg-> declaring an object with attributes and member functions inside a class;
 Encapsulation helps in data hiding through access modifiers
 eg-> private, protected, public
 private-> can't access from outside of that member/class;
 protected-> limited access from outside of that member/class;
 public->full access from outside of that class
-
+                    
+                        Constructor
+A special type function having name as same with class
+Three types of constructor->Parameterized, non parameterized, copy;
+Gets automatically called when object is created
+Can't call it whenever we want, can only call at the time of creating object
+Has power to set the values of the attributes of objects
+If we don't create our constructor, then compiler automatically creates one and calls it at the time of creating object
+so it's better to learn and create our self made constructor
+must be in public section of the class otherwise compiler can't access it from main function
 */
 
 int main(){
