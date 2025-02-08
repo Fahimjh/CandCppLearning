fd6d5c82697e24c0469a5080fb101e018f1ffcb2
fahimjh001 <fahimjh001@gmail.com>
2025-02-09 03:06:49 +0600

    data type modifiers added in apnadsa1

---
+++ apnadsa1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(string)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(long long int)<<endl;
+    //cout<<sizeof(short float)<<endl;
+    //cout<<sizeof(long long float)<<endl;
+    cout<<sizeof(long double)<<endl;
+    //cout<<sizeof(short double)<<endl;
+    //cout<<sizeof(long long double)<<endl;
+    return 0;
+}
