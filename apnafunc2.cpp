1eac5abba680eb680e36d5dabd9801ac411adee7
fahimjh001 <fahimjh001@gmail.com>
2025-02-08 12:40:08 +0600

    {

---
+++ apnafunc2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact=1;
+    while(n>0){
+        fact=fact*n;
+        n--;
+    }    
+    return fact;
+}
+int binomialCoefficient(int n, int r){
+    int nr=n-r;
+    return factorial(n)/(factorial(r)*factorial(nr));
+
+}
+
+int main(){
+    cout<<binomialCoefficient(8,2)<<endl;
+    cout<<binomialCoefficient(6,3)<<endl;
+}
