6b623303b14b5e1e3be2d7d3db70ae2a61649eb4
fahimjh001 <fahimjh001@gmail.com>
2025-02-09 11:45:48 +0600

    min and max by builtinfunction with position added in apnadsa8a

--- apnadsa8a.cpp
+++ apnadsa8a.cpp
@@ -4,13 +4,17 @@ using namespace std;
 //int findMin()
 
 int main(){
-    int arr[5]={25,7,12,-4,69},smallest=INT8_MAX,biggest=INT_FAST8_MIN;
+    int arr[5]={69,25,7,12,-4},smallest=INT8_MAX,biggest=INT_FAST8_MIN;
+    int ind4min,ind4max;
 
     for(int i=0;i<5;i++){
         smallest=min(arr[i],smallest);
+        if(smallest==arr[i])ind4min=i;
         biggest=max(arr[i],biggest);
+        if(biggest==arr[i])ind4max=i;
     }
-    cout<<smallest<<endl;
-    cout<<biggest<<endl;
+    cout<<smallest<<" POs: "<<ind4min<<endl;
+    cout<<biggest<<" POs: "<<ind4max<<endl;
+    //cout<<biggest<<endl;
     return 0;
 }
