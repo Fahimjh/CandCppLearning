8957f1d0e030595ca54518c6bcf70378a1c9f4db
fahimjh001 <fahimjh001@gmail.com>
2025-02-09 12:02:49 +0600

    min and max by builtinfunction with position and pass by reference  added in apnadsa8a

--- apnadsa8a.cpp
+++ apnadsa8a.cpp
@@ -1,20 +1,32 @@
 #include<iostream>
 using namespace std;
 
-//int findMin()
-
-int main(){
-    int arr[5]={69,25,7,12,-4},smallest=INT8_MAX,biggest=INT_FAST8_MIN;
-    int ind4min,ind4max;
-
+int findMin(int arr[]){
+    int smallest=INT8_MAX,ind4min;
     for(int i=0;i<5;i++){
         smallest=min(arr[i],smallest);
         if(smallest==arr[i])ind4min=i;
+    }
+    cout<<"Pos: "<<ind4min;
+    return smallest;
+}
+int findMax(int arr[]){
+    int biggest=INT8_MIN,ind4max;
+    for(int i=0;i<5;i++){
         biggest=max(arr[i],biggest);
         if(biggest==arr[i])ind4max=i;
     }
-    cout<<smallest<<" POs: "<<ind4min<<endl;
-    cout<<biggest<<" POs: "<<ind4max<<endl;
+    arr[ind4max]=96;
+    cout<<"Pos: "<<ind4max;
+    return biggest;
+}
+
+int main(){
+    int arr[5]={69,25,7,12,-4};
+    cout<<" Min Value: "<<findMin(arr)<<endl;
+    cout<<" Max Value: "<<findMax(arr)<<endl;
+    //cout<<biggest<<" POs: "<<ind4max<<endl;
     //cout<<biggest<<endl;
+    cout<<arr[0]<<endl;
     return 0;
 }
