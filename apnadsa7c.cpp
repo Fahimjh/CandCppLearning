759f3dc5fb503dbd27fbf748f71721b61f162bd6
fahimjh001 <fahimjh001@gmail.com>
2025-02-21 09:38:50 +0600

    first homework of biwise operators has been done

--- apnadsa7c.cpp
+++ apnadsa7c.cpp
@@ -1,16 +1,10 @@
+//bitwise operators
 #include<iostream>
 using namespace std;
 
-bool powOf2(int n){
-    if (n <= 0) return false; // 0 and negative numbers are not powers of 2
-    return (n & (n - 1)) == 0; // A power of 2 has only one bit set
-}
-
 int main(){
-    int num;
-    // cout << "Enter a number: ";
-    // cin >> num;
-    //cout << (powOf2(num) ? "True" : "False") << endl;
-    cout<<5&4;
-
+    int n=6,i=10;
+    cout<<(n&i)<<endl;
+    cout<<(n|i)<<endl;
+    cout<<(n^i)<<endl;
 }
