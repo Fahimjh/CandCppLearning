38f78a6348e3a5cdf2f086086f621b4b7f08d496
fahimjh001 <fahimjh001@gmail.com>
2025-02-21 10:11:56 +0600

    2nd homework-if a number is power of 2 added

--- apnadsa7c.cpp
+++ apnadsa7c.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main(){
-    int n=6,i=10;
-    cout<<(n&i)<<endl;
-    cout<<(n|i)<<endl;
-    cout<<(n^i)<<endl;
+    int n,count=0;
+    cout<<"Enter number: ";
+    cin>>n;
+    while(1){
+        if(n/2==1){
+            count==0;
+            break;
+        }
+        else if(n%2!=0){
+            //cout<<"else if n%2="<<n%2<<endl;
+            count++;
+            break;
+        }
+        n=n/2;
+    }
+    if(count==0){
+        cout<<"True";
+    }
+    else
+        cout<<"False";
 }
