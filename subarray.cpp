fc5686049c00b27f7d37dd917de9860da70e9983
fahimjh001 <fahimjh001@gmail.com>
2025-02-23 09:58:21 +0600

    subarray maximum sum found using brute force method

--- subarray.cpp
+++ subarray.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    int arr[5]={1,2,3,4,5},n=5;
+    int arr[7]={3,-4,5,4,-1,7,-8},n=7,sum,maxSum=0;
     for(int st=0;st<n;st++){
         for(int end=st;end<n;end++){
+            sum=0;
             for(int i=st;i<=end;i++){
                 cout<<arr[i];
+                sum=sum+arr[i];
+            }
+            if(sum>maxSum){
+                maxSum=sum;
             }
             cout<<" ";
         }
         cout<<endl;
     }
+    cout<<"Subarray maximum sum: "<<maxSum;
 }
