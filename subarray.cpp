44ea11af44e239587fe96d2d871ddea7de86ff21
fahimjh001 <fahimjh001@gmail.com>
2025-02-23 10:28:55 +0600

    subarray maximum sum found using kadane's algorithm and reducing complexity by n^1

--- subarray.cpp
+++ subarray.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main(){
-    int arr[7]={3,-4,5,4,-1,7,-8},n=7,sum,maxSum=0;
+    int arr[7]={3,-4,5,4,-1,7,-8},n=7,sum=0,maxSum=0;
     for(int st=0;st<n;st++){
-        sum=0;
-        for(int end=st;end<n;end++){
-            sum=sum+arr[end];
-            if(sum>maxSum){
-                maxSum=sum;
-            }
+        sum=sum+arr[st];
+        maxSum=max(sum,maxSum);
+        if(sum<0){
+            sum=0;
         }
     }
     cout<<"Subarray maximum sum: "<<maxSum;
