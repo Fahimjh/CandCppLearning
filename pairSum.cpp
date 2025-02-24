a0f76802e073b8178b15a1a0e9dce7a04d777d25
fahimjh001 <fahimjh001@gmail.com>
2025-02-24 23:32:48 +0600

    array pairSum by bruteforce addedwith modificatoin

--- pairSum.cpp
+++ pairSum.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main(){
-    int arr[4]={7,2,11,15},target=13;
+    int arr[4]={7,2,11,15},target=22;
     // vector<int>vec={7,2,11,15};
-    for(int i=0;i<4;i++){
-        if(arr[i]+arr[i+1]==target){
-            cout<<"values are: "<<arr[i]<<" and "<<arr[i+1]<<endl;
-            break;
+    for(int i=0;i<3;i++){
+        for(int j=i+1;j<4;j++){
+            if(arr[i]+arr[j]==target){
+                cout<<"values are: "<<arr[i]<<" and "<<arr[j]<<endl;
+                break;
+            }
         }
     }
 }
