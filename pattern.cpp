851567a17da65336bebd35086cde5321d8b3a33c
fahimjh001 <fahimjh001@gmail.com>
2025-03-03 00:47:12 +0600

    subarray sum added with less complexity

--- pattern.cpp
+++ pattern.cpp
@@ -3,14 +3,35 @@
 using namespace std;
 
 int main(){
-    vector <int> vec={1,2,3,4,5};
-    for(int i=0;i<5;i++){
-        for(int j=i;j<5;j++){
-            for(int k=i;k<=j;k++){
-                cout<<vec[k];
-            }
-            cout<<" ";
+    vector <int> vec={-2};
+    int sum=0,maxSum=INT8_MIN;
+    // for(int i=0;i<5;i++){
+        //     for(int j=i;j<5;j++){
+            //         sum=0;
+    //         for(int k=i;k<=j;k++){
+    //             cout<<vec[k];
+    //             sum=sum+vec[k];
+    //         }
+    //         if(sum>maxSum){
+        //             maxSum=sum;
+    //         }
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    for(int i=0;i<vec.size();i++){
+        sum=0;
+        for(int j=0;j<=i;j++){
+            cout<<vec[j]<<" ";
+            sum=sum+vec[j];
         }
+        maxSum=max(maxSum,sum);
         cout<<endl;
     }
+    // for(int i=0;i<vec.size();i++){
+    //     if(sum<0)sum=0;
+    //     sum=sum+vec[i];
+    //     maxSum=max(maxSum,sum);
+    // }
+    cout<<maxSum<<endl;
 }
