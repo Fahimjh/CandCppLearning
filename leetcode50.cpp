b2659d933d46b3d498eb9e7d765b282c0dd6d6ab
fahimjh001 <fahimjh001@gmail.com>
2025-03-11 23:34:11 +0600

    binary exponentiation for power using O(logn) added

---
+++ leetcode50.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+double myPow(double x, int n) {
+        long binForm=n;
+        if(n<0){
+            x=1/x;
+            binForm=-binForm;
+        }
+
+        double ans=1;
+
+        while(binForm>0){
+            if(binForm%2==1){
+                ans=ans*x;
+            }
+            x=x*x;
+            binForm=binForm/2;
+        }
+        return ans;
+}
+
+int main(){
+    int n;
+    double x;
+    cin>>x>>n;
+    cout<<myPow(x,n);
+    return 0;
+}
