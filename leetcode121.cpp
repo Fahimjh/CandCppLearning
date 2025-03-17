be17ceb4ea5eedba82aceb5ce70981afc5a73d00
fahimjh001 <fahimjh001@gmail.com>
2025-03-17 11:52:47 +0600

    leet121 stock buy and sell added

--- leetcode121.cpp
+++ leetcode121.cpp
@@ -1,31 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    int arr[]={7,6,4,3,1};
-    int maxProfit=0,bestBuy=arr[0];
-    //cout<<sizeof(arr)/sizeof(arr[0])<<endl;
-    // while(i<j){
-    //     if(arr[i+1]<min){
-    //         min=arr[i+1];
-    //         i++;
-    //     }
-    //     else i++;
-    //     if(arr[j-1]>arr[j]){
-    //         max=arr[j-1];
-    //         j--;
-    //         diff=max-min;
-    //     }
-    //     else j--;
-    //     if(i==j-1&&diff<0){
-    //         diff=0;
-    //     }
-    // }
-    for(int i=1;i<4;i++){
-        if(bestBuy<arr[i]){
-            maxProfit=max(maxProfit,arr[i]-bestBuy);
+    vector<int>prices={7,6,4,3,1};
+    int min=prices[0],mp=0,i;
+    //cout<<min<<max<<endl;
+    for(i=0; i<prices.size(); i++){
+        if(prices[i]<min){
+            min=prices[i];
+            //cout<<"min: "<<min;
         }
-        bestBuy=min(bestBuy,arr[i]);
+        //cout<<" i: "<<i<<endl;
+        mp=max(mp,prices[i]-min);
     }
-    cout<<maxProfit;
+    cout<<mp;
+    return 0;
 }
