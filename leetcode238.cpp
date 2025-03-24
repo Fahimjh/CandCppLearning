58770a137d2c55d04b9d37b74e210c0edf134a5f
fahimjh001 <fahimjh001@gmail.com>
2025-03-25 01:07:22 +0600

    leet 238 solved by optimal o(n)

--- leetcode238.cpp
+++ leetcode238.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 int main(){
-    vector<int>nums={-1,1,0,-3,3};
+    vector<int>nums={4,3,2,1,2};
     vector<int>answer=nums;
-    int prod;
+    int presuf=1;
+    answer[0]=1;
     for(int i=0;i<answer.size();i++){
-        prod=1;
-        for(int j=0;j<answer.size();j++){
-            if(j!=i){
-                prod*=nums[j];
-            }
-        }
-        answer[i]=prod;
+        presuf=presuf*nums[i];
+        answer[i+1]=presuf;
+        cout<<"answer["<<i<<"]="<<answer[i]<<endl;
+    }
+    presuf=1;cout<<"new"<<endl;
+    for(int i=answer.size()-2;i>=0;i--){
+        presuf=presuf*nums[i+1];
+        answer[i]=presuf*answer[i];
         cout<<"answer["<<i<<"]="<<answer[i]<<endl;
     }
 }
