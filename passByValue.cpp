01124df0ec9cbef072a07cceb348a7e5af05574b
fahimjh001 <fahimjh001@gmail.com>
2025-04-12 23:49:45 +0600

    pass by reference(elias/address operator)

--- passByValue.cpp
+++ passByValue.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void changeA(int* a){
-    *a=20;
+void changeA(int &a){
+    a=20;
 }
 
 int main(){
     int a=10;
     cout<<"value of a before passing: "<<a<<endl;
-    changeA(&a);
+    changeA(a);
     cout<<"value of a after passing: "<<a<<endl;
 }
