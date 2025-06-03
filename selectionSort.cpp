6d01de95d8f036f2b89416b3de4d47b6595b0f0e
fahimjh001 <fahimjh001@gmail.com>
2025-06-03 19:55:29 +0600

    selection sort added with ascending and descending

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,25 +1,55 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int arr[],int n){
+void ascendingSelectionSort(int arr[5], int n){
     for(int i=0;i<n;i++){
-        int minVal=i;
-        for(int j=i+1;j<n;j++){
-            if(arr[minVal]>arr[j]){
+        for(int j=i;j<n;j++){
+            if(arr[j]<arr[i]){
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+}
+void descendingSelectionSort(int arr[5], int n){
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[j]>arr[i]){
                 swap(arr[i],arr[j]);
             }
         }
     }
 }
 
-void printArr(int arr[],int n){
+void printArr(int arr[5],int n){
+    cout<<"Your array: ";
     for(int i=0;i<n;i++){
-        cout<<"arr["<<i<<"]="<<arr[i]<<endl;
+        cout<<"arr["<<i<<"] = "<<arr[i]<<endl;
     }
 }
 
 int main(){
-    int arr[5]={5,4,3,2,1},n=5;
-    selectionSort(arr,n);
+    int arr[5],n,c;
+    cout<<"Enter array size: ";
+    cin>>n;
+    cout<<"Enter array Values: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    cout<<"Before sorting: ";
     printArr(arr,n);
+
+    cout<<"Press 1 for ascending: ";
+    cin>>c;
+    if(c==1){
+        ascendingSelectionSort(arr,n);
+    }
+    else{
+        descendingSelectionSort(arr,n);
+    }
+
+    cout<<"After sorting: ";
+    printArr(arr,n);
+
+    return 0;
 }
