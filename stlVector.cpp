5733a3d63056538a27f0f8f6d0bc686bb5e405a9
fahimjh001 <fahimjh001@gmail.com>
2025-06-23 12:34:22 +0600

    vector o^n2 operations learned and added

--- stlVector.cpp
+++ stlVector.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 
 int main(){
-    vector<int>vec={1,2,3};
-    vec.push_back(1);
-    vec.push_back(1);
-    vec.push_back(1);
-    vec.push_back(1);
+    vector<int>vec={1,2,3,6,7,8,9,4,5};
     cout<<"Vector size: "<<vec.size()<<endl;
     cout<<"Vector capacity: "<<vec.capacity()<<endl;
+    vec.erase(vec.begin()+3,vec.end()-2);
+    cout<<"Vector size: "<<vec.size()<<endl;
+    cout<<"Vector capacity: "<<vec.capacity()<<endl;
+    cout<<"Vector isEmpty: "<<vec.empty()<<endl;
+    for(int i=0;i<vec.size();i++){
+        cout<<vec[i]<<endl;
+    }
+    vec.clear();
+    cout<<"Vector size: "<<vec.size()<<endl;
+    cout<<"Vector capacity: "<<vec.capacity()<<endl;
+    cout<<"Vector isEmpty: "<<vec.empty()<<endl;
 }
