52addd067243f13827ace8273c8f5ece2f7963c5
fahimjh001 <fahimjh001@gmail.com>
2025-06-24 16:55:37 +0600

    stl queue and pririty queue normal and reverese added

--- stlQueue.cpp
+++ stlQueue.cpp
@@ -3,16 +3,36 @@
 using namespace std;
 
 int main(){
-    queue<int>q1,q2;
-    q1.push(1);
-    q1.push(2);
-    q1.push(3);
-    q2=q1;
-    cout<<"q1 size: "<<q1.size()<<endl;
-    cout<<"q2 size: "<<q2.size()<<endl;
-    while(!q1.empty()){
-        cout<<q1.front()<<" ";
-        q1.pop();
+    // queue<int>q1,q2;
+    // q1.push(1);
+    // q1.push(2);
+    // q1.push(3);
+    // q2=q1;
+    // cout<<"q1 size: "<<q1.size()<<endl;
+    // cout<<"q2 size: "<<q2.size()<<endl;
+    // while(!q1.empty()){
+    //     cout<<q1.front()<<" ";
+    //     q1.pop();
+    // }
+    // cout<<endl;
+    priority_queue<int>pq;
+    pq.push(5);
+    pq.push(3);
+    pq.push(10);
+    pq.push(4);
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    cout<<endl;
+    priority_queue<int,vector<int>,greater<int>>pqr;
+    pqr.push(5);
+    pqr.push(3);
+    pqr.push(10);
+    pqr.push(4);
+    while(!pqr.empty()){
+        cout<<pqr.top()<<" ";
+        pqr.pop();
     }
     cout<<endl;
 }
